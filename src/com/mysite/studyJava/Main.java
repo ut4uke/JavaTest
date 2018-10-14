87e717602894a4097484fd6504c641d1d4214b4b
borman <ut4uke@ukr.net>
2018-10-14 15:08:38 +0300

    second commit

--- src/com/mysite/studyJava/Main.java
+++ src/com/mysite/studyJava/Main.java
@@ -3,6 +3,6 @@ package com.mysite.studyJava;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!Hello world!");
     }
 }
